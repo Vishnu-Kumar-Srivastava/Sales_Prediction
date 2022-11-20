4f394c8deb6dad27e17e94923277be58d310e384
Bhavya <bhavya,2k03@gmail.com>
2022-11-20 22:51:26 +0530

    integrated ml model

--- project/views.py
+++ project/views.py
@@ -79,10 +79,17 @@ def upload(request):
    return redirect('upload_file')
 
 def upload_file(request):
+   print("1\n\n")
    def upload_file(request):
+      print(2)
+      print()
       if request.method == "POST":
         my_uploaded_file = request.FILES['my_uploaded_file'].read() # get the uploaded file
+        print(3)
+        print()
         f=open(my_uploaded_file)
+        print(4)
+        print()
         print(f.readlines())
         return render(request,"<h1> Successful</h1>")
                      
@@ -108,182 +115,183 @@ def indexx(request):
          #know where to save the file
          d = os.getcwd() #current directory of the project
          file_directory = d+'\media\\'+name
+         ML(request.user.email)
          return redirect(index)
       
    return render(request, 'indexx.html')
 
-   def ML(request):
-      df=pd.read_csv("Sales_final.csv.")
+def ML(email):
+   df=pd.read_csv(r"C:\Users\DELL\Desktop\software project\Sales_Prediction\media\Sales_final.csv")
 
-      df.columns=["Date","Holiday","Avg. % ad spend of gross revenue","Avg. % discount","Sales"]
+   df.columns=["Date","Holiday","Avg. % ad spend of gross revenue","Avg. % discount","Sales"]
 
-      train_length=len(df["Sales"])-df["Sales"].isnull().sum()
+   train_length=len(df["Sales"])-df["Sales"].isnull().sum()
 
-      test_lenth=df["Sales"].isnull().sum()
+   test_lenth=df["Sales"].isnull().sum()
 
-      le=preprocessing.LabelEncoder()
-      encoded=le.fit_transform(df["Holiday"])
+   le=preprocessing.LabelEncoder()
+   encoded=le.fit_transform(df["Holiday"])
 
-      from sklearn.preprocessing import MinMaxScaler
-      scaler=MinMaxScaler()
+   from sklearn.preprocessing import MinMaxScaler
+   scaler=MinMaxScaler()
 
-      scaled_ad_spend=scaler.fit_transform(df["Avg. % ad spend of gross revenue"][:,np.newaxis])
+   scaled_ad_spend=scaler.fit_transform(df["Avg. % ad spend of gross revenue"][:,np.newaxis])
 
-      scaled_discount=scaler.fit_transform(df["Avg. % discount"][:,np.newaxis])
-      scaled_sales= scaler.fit_transform(df["Sales"][:,np.newaxis])
+   scaled_discount=scaler.fit_transform(df["Avg. % discount"][:,np.newaxis])
+   scaled_sales= scaler.fit_transform(df["Sales"][:,np.newaxis])
 
-      scaled_discount2=np.array(scaled_discount)
-      scaled_sales2=np.array(scaled_sales)
-      encoded=np.array(encoded)
+   scaled_discount2=np.array(scaled_discount)
+   scaled_sales2=np.array(scaled_sales)
+   encoded=np.array(encoded)
 
-      encoded=encoded.reshape((len(df["Sales"]),1))
+   encoded=encoded.reshape((len(df["Sales"]),1))
 
-      final3=np.array((encoded[:train_length],scaled_ad_spend[:train_length],scaled_discount[:train_length]))
+   final3=np.array((encoded[:train_length],scaled_ad_spend[:train_length],scaled_discount[:train_length]))
 
-      test=np.array((encoded[train_length:],scaled_ad_spend[train_length:],scaled_discount[train_length:]))
-      # print(test.shape)
+   test=np.array((encoded[train_length:],scaled_ad_spend[train_length:],scaled_discount[train_length:]))
+   # print(test.shape)
 
-      test=test.reshape((test_lenth,3))
+   test=test.reshape((test_lenth,3))
 
-      final3=final3.reshape((train_length,3))
+   final3=final3.reshape((train_length,3))
 
-      from keras.preprocessing.sequence import TimeseriesGenerator
-      n_input=3
-      n_features=3
-      generator=TimeseriesGenerator(final3,scaled_sales[:train_length],length=n_input,batch_size=1)
+   from keras.preprocessing.sequence import TimeseriesGenerator
+   n_input=3
+   n_features=3
+   generator=TimeseriesGenerator(final3,scaled_sales[:train_length],length=n_input,batch_size=1)
 
-      model = Sequential()
-      model.add(LSTM(100, activation='relu', input_shape=(n_input,n_features)))
-      model.add(Dense(1))
-      model.summary()
+   model = Sequential()
+   model.add(LSTM(100, activation='relu', input_shape=(n_input,n_features)))
+   model.add(Dense(1))
+   model.summary()
 
-      model.compile(loss=tf.keras.losses.mse,optimizer=tf.keras.optimizers.Adam(lr=0.001))
-      model.fit(generator,epochs=150,batch_size=30)
+   model.compile(loss=tf.keras.losses.mse,optimizer=tf.keras.optimizers.Adam(lr=0.001))
+   model.fit(generator,epochs=20,batch_size=30)
 
-      first_eval_batch=final3[-n_input:,:,np.newaxis]
-      current_batch = first_eval_batch.reshape((1, n_input, n_features))
-      # print(first_eval_batch)
-      # print(current_batch)
+   first_eval_batch=final3[-n_input:,:,np.newaxis]
+   current_batch = first_eval_batch.reshape((1, n_input, n_features))
+   # print(first_eval_batch)
+   # print(current_batch)
 
-      final3=final3.astype(np.uint8)
+   final3=final3.astype(np.uint8)
 
-      test=test.astype(np.uint8)
+   test=test.astype(np.uint8)
 
-      predictions=[]
-      for i in range(0,test_lenth):
-         if i==0:
-            predictions.append(model.predict(current_batch)[0])
-            # print("hi")
-         elif i==1:
-            # print("pass")
-            # first_eval_batch=final3[-n_input+1:,:,np.newaxis]
-            first_eval_batch=final3[-n_input+1:,:,np.newaxis]
-            first_eval_batch=list(first_eval_batch)
-            # print(first_eval_batch)
-            first_eval_batch.append(test[0,:,np.newaxis])
-            # print(first_eval_batch)
-            first_eval_batch=np.array(first_eval_batch)
-            current_batch = first_eval_batch.reshape((1, n_input, n_features))
-            # print(current_batch)
-            predictions.append(model.predict(current_batch)[0])
-         elif i==2:
-            first_eval_batch=final3[-n_input+2:,:,np.newaxis]
-            first_eval_batch=list(first_eval_batch)
-            first_eval_batch.append(test[0,:,np.newaxis])
-            first_eval_batch.append(test[1,:,np.newaxis])
-            # print(first_eval_batch)
-            first_eval_batch=np.array(first_eval_batch)
+   predictions=[]
+   for i in range(0,test_lenth):
+      if i==0:
+         predictions.append(model.predict(current_batch)[0])
+         # print("hi")
+      elif i==1:
+         # print("pass")
+         # first_eval_batch=final3[-n_input+1:,:,np.newaxis]
+         first_eval_batch=final3[-n_input+1:,:,np.newaxis]
+         first_eval_batch=list(first_eval_batch)
+         # print(first_eval_batch)
+         first_eval_batch.append(test[0,:,np.newaxis])
+         # print(first_eval_batch)
+         first_eval_batch=np.array(first_eval_batch)
+         current_batch = first_eval_batch.reshape((1, n_input, n_features))
+         # print(current_batch)
+         predictions.append(model.predict(current_batch)[0])
+      elif i==2:
+         first_eval_batch=final3[-n_input+2:,:,np.newaxis]
+         first_eval_batch=list(first_eval_batch)
+         first_eval_batch.append(test[0,:,np.newaxis])
+         first_eval_batch.append(test[1,:,np.newaxis])
+         # print(first_eval_batch)
+         first_eval_batch=np.array(first_eval_batch)
 
-            current_batch = first_eval_batch.reshape((1, n_input, n_features))
-            # print(current_batch)
-            predictions.append(model.predict(current_batch)[0])
-         else:
-            first_eval_batch=test[i-n_input:i,:,np.newaxis]
-            current_batch = first_eval_batch.reshape((1, n_input, n_features))
-            # print(current_batch)
-            predictions.append(model.predict(current_batch)[0])
+         current_batch = first_eval_batch.reshape((1, n_input, n_features))
+         # print(current_batch)
+         predictions.append(model.predict(current_batch)[0])
+      else:
+         first_eval_batch=test[i-n_input:i,:,np.newaxis]
+         current_batch = first_eval_batch.reshape((1, n_input, n_features))
+         # print(current_batch)
+         predictions.append(model.predict(current_batch)[0])
 
-      true_predictions = scaler.inverse_transform(predictions)
+   true_predictions = scaler.inverse_transform(predictions)
 
-      Workbookname=np.random.randint(0,1000)
-      workbook = xlsxwriter.Workbook(f'{Workbookname}.xlsx')
-      worksheet = workbook.add_worksheet()
-      worksheet.write('A1', 'Date')
-      worksheet.write('B1', 'Sales')
+   Workbookname=np.random.randint(0,1000)
+   workbook = xlsxwriter.Workbook(f'{Workbookname}.xlsx')
+   worksheet = workbook.add_worksheet()
+   worksheet.write('A1', 'Date')
+   worksheet.write('B1', 'Sales')
 
-      row = 1
-      column = 0
-      for i in df["Date"][train_length:]:
+   row = 1
+   column = 0
+   for i in df["Date"][train_length:]:
+   
+      worksheet.write(row, column, i)
+      row += 1
+
+   row = 1
+   column +=1
+   for i in true_predictions :
+
+      worksheet.write(row, column, i)
+      row += 1
       
-         worksheet.write(row, column, i)
-         row += 1
+   workbook.close()
 
-         row = 1
-         column +=1
-      for i in true_predictions :
+   print("Done!")
+   import smtplib
+   from email.mime.multipart import MIMEMultipart
+   from email.mime.text import MIMEText
+   from email.mime.base import MIMEBase
+   from email import encoders
+   fromaddr = "predict.io.2k22@gmail.com"
+   toaddr = f"{email}"   
 
-         worksheet.write(row, column, i)
-         row += 1
+   msg = MIMEMultipart() 
+   msg['From'] = fromaddr
+   
+   # storing the receivers email address 
+   msg['To'] = toaddr
+   
+   # storing the subject 
+   msg['Subject'] = "Prediction results"
+   
+   # string to store the body of the mail
+   body = "Thank you for using our website, here are the results."
+   
+   # attach the body with the msg instance
+   msg.attach(MIMEText(body, 'plain'))
+   
+   # open the file to be sent 
+   filename = f"{Workbookname}.xlsx"
+   attachment = open(f'{Workbookname}.xlsx', "rb")
+
+   # instance of MIMEBase and named as p
+   p = MIMEBase('application', 'octet-stream')
+   
+   # To change the payload into encoded form
+   p.set_payload((attachment).read())
+   
+   # encode into base64
+   encoders.encode_base64(p)
+      
+   p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+   
+   # attach the instance 'p' to instance 'msg'
+   msg.attach(p)
+   
+   # creates SMTP session
+   s = smtplib.SMTP('smtp.gmail.com', 587)
+   
+   # start TLS for security
+   s.starttls()
+   
+   # Authentication
+   s.login(fromaddr, "merwuzzcsxlpzjve")
+   
+   # Converts the Multipart msg into a string
+   text = msg.as_string()
+   
+   # sending the mail
+   s.sendmail(fromaddr, toaddr, text)
+   
+   # terminating the session
+   s.quit()
          
-      workbook.close()
-
-      print("Done!")
-      import smtplib
-      from email.mime.multipart import MIMEMultipart
-      from email.mime.text import MIMEText
-      from email.mime.base import MIMEBase
-      from email import encoders
-      fromaddr = "predict.io.2k22@gmail.com"
-      toaddr = f"{request.user.email}"   
-
-      msg = MIMEMultipart() 
-      msg['From'] = fromaddr
-      
-      # storing the receivers email address 
-      msg['To'] = toaddr
-      
-      # storing the subject 
-      msg['Subject'] = "Prediction results"
-      
-      # string to store the body of the mail
-      body = "Thank you for using our website, here are the results."
-      
-      # attach the body with the msg instance
-      msg.attach(MIMEText(body, 'plain'))
-      
-      # open the file to be sent 
-      filename = f"{Workbookname}.xlsx"
-      attachment = open(f'{Workbookname}.xlsx', "rb")
-
-      # instance of MIMEBase and named as p
-      p = MIMEBase('application', 'octet-stream')
-      
-      # To change the payload into encoded form
-      p.set_payload((attachment).read())
-      
-      # encode into base64
-      encoders.encode_base64(p)
-         
-      p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-      
-      # attach the instance 'p' to instance 'msg'
-      msg.attach(p)
-      
-      # creates SMTP session
-      s = smtplib.SMTP('smtp.gmail.com', 587)
-      
-      # start TLS for security
-      s.starttls()
-      
-      # Authentication
-      s.login(fromaddr, "merwuzzcsxlpzjve")
-      
-      # Converts the Multipart msg into a string
-      text = msg.as_string()
-      
-      # sending the mail
-      s.sendmail(fromaddr, toaddr, text)
-      
-      # terminating the session
-      s.quit()
-            
